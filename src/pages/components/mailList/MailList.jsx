f353a06606fb554de292908a8a9e6d0304dfaf9f
Kamal Ahmed <kamalacca@gmail.com>
2022-07-14 01:48:51 +0600

    completed mailing list

--- src/pages/components/mailList/MailList.jsx
+++ src/pages/components/mailList/MailList.jsx
@@ -2,7 +2,14 @@ import './mailList.css'
 
 function MailList() {
   return (
-    <div>MailList</div>
+    <div className='mail'>
+        <h1 className="mailTitle">Save time, save money!</h1>
+        <span className="mailDesc">Sign up and we'll send the best deals to you</span>
+        <div className="mailInputContainer">
+            <input type="text" placeholder='Your Mail' />
+            <button>Subscribe</button>
+        </div>
+    </div>
   )
 }
 
